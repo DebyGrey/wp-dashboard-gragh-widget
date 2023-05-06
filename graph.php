a170913f0c610723ad844f7262cc004c741cabac
DebyGrey <demifashoro@gmail.com>
2023-05-06 15:16:00 +0100

    Update documentation

--- graph.php
+++ graph.php
@@ -6,7 +6,7 @@
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Abdulsalam Ishaq
+ * Author:            Deborah Fashoro
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       graph
